8b9a728f3ee47355300d73e810404b4fe1a1cfd8
Kamei132 <92843714+Kamei132@users.noreply.github.com>
2021-10-20 21:33:11 +0530

    Add files via upload

---
+++ kameimarksprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int english,maths,physics,chemistry;
+english=56;
+maths=89;
+physics=70;
+chemistry=77;
+printf("Marks obtained in english=%d\n",english);
+printf("Marks obtained in maths=%d\n",maths);
+printf("Marks obtained in physics=%d\n",physics);
+printf("Marks obtained in chemistry=%d\n",chemistry);
+printf("Total marks obtained=%d",english+maths+physics+chemistry);
+}
