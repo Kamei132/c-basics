7062abefabfeb7e7d6b2d7439d3a44f229dfe142
Kamei132 <92843714+Kamei132@users.noreply.github.com>
2021-10-20 11:50:30 +0530

    Add files via upload

--- kamei.cpp
+++ kamei.cpp
@@ -1,5 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
-	int a,b
+	int age;
+	float cgpa;
+	age=21;
+	cgpa=8.4;
+	printf("age of the studnet is %d\n",age);
+	printf("cgpa of the student is %f\n",cgpa);
 }
