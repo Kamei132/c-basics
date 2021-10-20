6d321281b878d19de53fa4d05c88cbdaf5b7fcf3
Kamei132 <92843714+Kamei132@users.noreply.github.com>
2021-10-20 12:20:21 +0530

    Add files via upload

---
+++ kamei 2.cpp
@@ -0,0 +1,17 @@
+/*c program to print info. of the student */
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//initialization
+	age=21;
+	cgpa=6.6;
+	initial='B';
+	//output
+	printf("age of the student is: %d\n",age);
+	printf("cgpa of the student is: %f\n",cgpa);
+	printf("student surname starts with: %c\n",initial);
+}
